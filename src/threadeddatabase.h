47754d127d0d2f7b9791a882f7696f1f97a44d7c
Jonah Brüchert <jbb@kaidan.im>
2023-03-24 18:28:56 +0100

    Add LicenseRef-KDE-Accepted-LGPL as license choice

--- src/threadeddatabase.h
+++ src/threadeddatabase.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Jonah Brüchert <jbb@kaidan.im>
 //
-// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 #pragma once
 
